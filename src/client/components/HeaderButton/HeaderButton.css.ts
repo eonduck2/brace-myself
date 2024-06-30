01eeb91a7abf5e8e7f0f33e6fa34611d12dae7b6
eonduck2 <whdtnxd@gmail.com>
2024-07-01 02:12:08 +0900

    :package: 이종수: 웹팩 컨피그 파일 수정

--- src/client/components/HeaderButton/HeaderButton.css.ts
+++ src/client/components/HeaderButton/HeaderButton.css.ts
@@ -1,10 +1,17 @@
-import { style } from "@vanilla-extract/css";
+import { style, globalStyle } from "@vanilla-extract/css";
 
-export const buttonStyle = style({
-  backgroundColor: "blue",
-  color: "white",
-  padding: "10px 20px",
-  borderRadius: "5px",
-  border: "none",
-  cursor: "pointer",
+// export const buttonStyle = style({
+//   backgroundColor: "blue",
+//   color: "white",
+//   padding: "10px 20px",
+//   borderRadius: "5px",
+//   border: "none",
+//   cursor: "pointer",
+// });
+
+export const myStyle = style({
+  display: "flex",
+  paddingTop: "3px",
 });
+
+// console.log(style({}));
