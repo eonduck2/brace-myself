3118b5625e9a5d8aeb6f0c8ab1b6e4ad89f9521a
eonduck2 <whdtnxd@gmail.com>
2024-07-02 09:44:50 +0900

    :wrench: 이종수: 테일윈드, 포스트 css 컨피그 파일 생성 및 정의

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeaderButton from "./components/header/headerButton/HeaderButton";
+import "../styles/output.css";
 
 const App: React.FC = () => {
   return <HeaderButton />;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-// const container = document.getElementById("app");
+// const container = document.getElementById('app');
 // const root = createRoot(container); // createRoot(container!) if you use TypeScript
 // root.render(<App tab="home" />);
